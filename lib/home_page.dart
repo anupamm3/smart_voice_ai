46955c2070d6e77c7a4f2e8ee6d84ec452778f98
Anupam Arya <anupamaryajsr@gmail.com>
2025-07-04 21:40:23 +0530

    feature box added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_voice_ai/feature_box.dart';
 import 'package:smart_voice_ai/pallete.dart';
 
 class HomePage extends StatefulWidget {
@@ -44,7 +45,64 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           ),
-          const SizedBox(height: 20),
+          //Greeting Text
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Pallete.borderColor,
+              ),
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'How can I help you?',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  fontSize: 25,
+                  color: Pallete.mainFontColor,
+                ),
+              ),
+            ),
+          ),
+          //Feature Text
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(10),
+            margin: const EdgeInsets.only(top: 10, left: 22),
+            child: const Text('Here are a few features you can try:',
+              style: TextStyle(
+                fontFamily: 'Cera Pro',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Pallete.mainFontColor,
+              ),
+            ),
+          ),
+          //Feature Boxes
+          Column(
+            children: [
+              FeatureBox(
+                color: Pallete.featureBox1Color,
+                headerText: 'ChatGPT',
+                descriptionText: 'A smarter way to organize your thoughts and ideas with ChatGPT',
+              ),
+              FeatureBox(
+                color: Pallete.featureBox2Color,
+                headerText: 'Dall-E',
+                descriptionText: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.featureBox3Color,
+                headerText: 'Smart Voice Assistant',
+                descriptionText: 'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              ),
+            ],
+          ),
         ],
       ),
     );
--- lib/pallete.dart
+++ lib/pallete.dart
@@ -2,10 +2,9 @@ import 'package:flutter/material.dart';
 
 class Pallete {
   static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
-  static const Color firstSuggestionBoxColor = Color.fromRGBO(165, 231, 244, 1);
-  static const Color secondSuggestionBoxColor =
-      Color.fromRGBO(157, 202, 235, 1);
-  static const Color thirdSuggestionBoxColor = Color.fromRGBO(162, 238, 239, 1);
+  static const Color featureBox1Color = Color.fromRGBO(165, 231, 244, 1);
+  static const Color featureBox2Color = Color.fromRGBO(157, 202, 235, 1);
+  static const Color featureBox3Color = Color.fromRGBO(162, 238, 239, 1);
   static const Color assistantCircleColor = Color.fromRGBO(209, 243, 249, 1);
   static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
   static const Color blackColor = Colors.black;
